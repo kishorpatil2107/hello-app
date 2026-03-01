1e1aca809d8435892169b1442bdc7f4f8eb26036
Ubuntu <ubuntu@ip-172-31-27-106.eu-west-1.compute.internal>
2026-03-01 20:39:00 +0000

    app.java modified

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,7 +1,9 @@
 package com.devops;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello DevOps from Docker!");
+
+        Thread.currentThread().join();
     }
 }
